6c9992871dbab43a0e57484ed385b73ef33d1593
mthdht <mthdht@gmail.com>
2018-05-24 16:23:56 +0200

    add function for have search bar in navigation menu

--- wp-content/themes/strasbourg/functions.php
+++ wp-content/themes/strasbourg/functions.php
@@ -4,3 +4,14 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'admin-style', get_template_directory_uri() . '/css/admin-style.css' );
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+function add_search_box($items, $args) {
+    ob_start();
+    get_search_form();
+    $searchform = ob_get_contents();
+    ob_end_clean();
+
+    $items .= '<li>' . $searchform . '</li>';
+    return $items;
+}
+add_filter('wp_nav_menu_items','add_search_box', 10, 2);
